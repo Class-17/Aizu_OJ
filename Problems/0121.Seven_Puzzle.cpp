86a88c9f7ce2c4a8db027b34d849ff463ca248ab
Class-17 <verfolgen17@gmail.com>
2021-11-09 17:38:17 +0800

    0121.Seven_Puzzle Solved

--- Problems/0121.Seven_Puzzle.cpp
+++ Problems/0121.Seven_Puzzle.cpp
@@ -1,58 +1,38 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
 #include <queue>
-#include <unordered_set>
+#include <map>
 #include <algorithm>
 using namespace std;
 
 const int dirs[] = {1, -1, 4, -4};
 
-int find_zero(string &seq) {
-    for (int i = 0; i < 8; ++i)
-        if (seq[i] == '0') return i;
-    return -1;
-}
-
-int bfs(string &seq) {
-    queue<string> q;
-    unordered_set<string> memo;
-    q.push(seq);
-    memo.insert(seq);
-    int dist = 0;
-    while (!q.empty()) {
-        int len = q.size();
-        while (len--) {
-            string x = q.front();
-            q.pop();
-            printf("%s\n", x.c_str());
-            if (x == "01234567") return dist;
-            int p = find_zero(x);
-            for (int i = 0; i < 4; ++i) {
-                int np = p + dirs[i];
-                if (np < 0 || np >= 8) continue;
-                swap(x[p], x[np]);
-                if (memo.find(x) == memo.end()) {
-                    q.push(x);
-                    memo.insert(x);
-                }
-                swap(x[p], x[np]);
-            }
-        }
-        dist++;
-    }
-    return -1;
-}
-
 int main() {
-    string seq = "01234567";
-    char x, _;
-    while (scanf("%c%c", &x, &_)) {
-        seq[0] = x;
-        for (int i = 1; i < 8; ++i) {
-            scanf("%c%c", &x, &_);
-            seq[i] = x;
+    queue<string> q;
+    map<string, int> dp;
+    q.push("01234567");
+    dp["01234567"] = 0;
+    while (!q.empty()) {
+        string x = q.front();
+        q.pop();
+        int p = x.find('0');
+        for (int i = 0; i < 4; ++i) {
+            int np = p + dirs[i];
+            if (np < 0 || np >= 8 || p == 3 && np == 4 || p == 4 && np == 3) continue;
+            int dist = dp[x] + 1;
+            swap(x[p], x[np]);
+            if (dp.find(x) == dp.end()) {
+                dp[x] = dist;
+                q.push(x);
+            }
+            swap(x[p], x[np]);
         }
-        printf("%d\n", bfs(seq));
+    }
+    string seq = "01234567";
+    while (cin >> seq[0]) {
+        for (int i = 1; i < 8; ++i)
+            cin >> seq[i];
+        cout << dp[seq] << endl;
     }
     return 0;
 }
